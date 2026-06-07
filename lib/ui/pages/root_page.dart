b54cc450d4f8a7e05f02b85bffe1c542a4fd7c4c
cuixinyuan666 <98614574+cuixinyuan666@users.noreply.github.com>
2026-06-07 23:39:23 +0800

    Remove CZSC backend tab from vespa_tdx UI

--- lib/ui/pages/root_page.dart
+++ lib/ui/pages/root_page.dart
@@ -1,42 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'czsc_easy_tdx_page.dart';
 import 'replay_page.dart';
 
-class RootPage extends StatefulWidget {
+class RootPage extends StatelessWidget {
   const RootPage({super.key});
 
   @override
-  State<RootPage> createState() => _RootPageState();
-}
-
-class _RootPageState extends State<RootPage> {
-  int _index = 0;
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: _index,
-        children: const [
-          ReplayPage(),
-          CzscEasyTdxPage(),
-        ],
-      ),
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: _index,
-        onDestinationSelected: (value) => setState(() => _index = value),
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.timeline),
-            label: '本地复盘',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.hub),
-            label: 'CZSC后端',
-          ),
-        ],
-      ),
-    );
+    return const ReplayPage();
   }
 }
