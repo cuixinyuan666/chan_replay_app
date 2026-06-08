05441fcb7c6b0986e94961247a7e0a9d72cc5713
cuixinyuan666 <98614574+cuixinyuan666@users.noreply.github.com>
2026-06-07 21:09:35 -0700

    Remove replay and compare tabs

--- lib/ui/pages/root_page.dart
+++ lib/ui/pages/root_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'chanpy_compare_page.dart';
 import 'replay_page.dart';
 
 class RootPage extends StatelessWidget {
@@ -8,29 +7,6 @@ class RootPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        backgroundColor: const Color(0xFF0B0D10),
-        appBar: AppBar(
-          toolbarHeight: 40,
-          elevation: 0,
-          backgroundColor: const Color(0xFF131722),
-          title: const Text('缠论复盘', style: TextStyle(fontSize: 15)),
-          bottom: const TabBar(
-            tabs: [
-              Tab(text: '本地复盘'),
-              Tab(text: 'Vespa对齐'),
-            ],
-          ),
-        ),
-        body: const TabBarView(
-          children: [
-            ReplayPage(),
-            ChanpyComparePage(),
-          ],
-        ),
-      ),
-    );
+    return const ReplayPage();
   }
 }
