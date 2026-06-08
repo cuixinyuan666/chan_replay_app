2ae57a586e307e15d8bf8986116080d44aada839
cuixinyuan666 <98614574+cuixinyuan666@users.noreply.github.com>
2026-06-08 16:49:51 -0700

    Move TradingView toolbox out of root page

--- lib/ui/pages/root_page.dart
+++ lib/ui/pages/root_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../drawing/tradingview_toolbox_host.dart';
 import 'origin_replay_page_v2.dart';
 
 class RootPage extends StatelessWidget {
@@ -8,12 +7,6 @@ class RootPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const TradingViewToolboxHost(
-      // 当前小任务先把 TradingView 工具箱入口接入根页面。
-      // 画线落点与 ChanSnapshot 动态可用状态将在下一小任务移入图表状态层绑定。
-      hasBars: true,
-      hasChanSnapshot: false,
-      child: OriginReplayPageV2(),
-    );
+    return const OriginReplayPageV2();
   }
 }
